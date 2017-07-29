2971e4c65eec9a69e0b18d5c6978e5ee61cb8880
Gabriel Schenker <gabriel.schenker@docker.com>
2017-07-29 14:10:21 -0700

    Added edit-and-continue exercise

---
+++ edit-and-continue/api/src/main/java/com/docker/ddev/service/ImageService.java
@@ -0,0 +1,10 @@
+
+package com.docker.ddev.service;
+
+import java.util.List;
+import com.docker.ddev.model.Image;
+
+public interface ImageService {
+	List<Image> findAllImages();
+	Image findById(Long productId);
+}
---
+++ edit-and-continue/api/src/main/java/com/docker/ddev/util/CustomErrorType.java
@@ -0,0 +1,12 @@
+
+package com.docker.ddev.util;
+
+public class CustomErrorType {
+    private String errorMessage;
+    public CustomErrorType(String errorMessage){
+        this.errorMessage = errorMessage;
+    }
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
