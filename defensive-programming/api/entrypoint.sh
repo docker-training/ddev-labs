94d1d4a7978cd0dfe0807093bdf94be15eb91c99
Gabriel Schenker <gabriel.schenker@docker.com>
2017-07-30 17:16:12 -0700

    Added defensive programming exercise

---
+++ defensive-programming/api/entrypoint.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+echoerr() { if [[ $QUIET -ne 1 ]]; then echo "$@" 1>&2; fi }
+
+wait_for()
+{
+    if [[ $TIMEOUT -gt 0 ]]; then
+        echoerr "$cmdname: waiting $TIMEOUT seconds for $HOST:$PORT"
+    else
+        echoerr "$cmdname: waiting for $HOST:$PORT without a timeout"
+    fi
+    start_ts=$(date +%s)
+    while :
+    do
+        echo "Probing  $HOST:$PORT..."
+        nc -z -w 1 $HOST $PORT </dev/null
+        result=$?
+        end_ts=$(date +%s)
+        delta_ts=$((end_ts-start_ts))
+        if [[ $result -eq 0 ]]; then
+            echoerr "$cmdname: $HOST:$PORT is available after $((end_ts - start_ts)) seconds"
+            break
+        fi
+        echo "Passed $delta_ts of $TIMEOUT seconds"
+        if [[ $delta_ts -ge $TIMEOUT ]]; then
+            echo "Timeout reached, aborting..."
+            break
+        fi
+        sleep 1
+    done
+    return $result
+}
+
+TIMEOUT=${TIMEOUT:-15}
+QUIET=${QUIET:-0}
+
+if [[ "$HOST" == "" || "$PORT" == "" ]]; then
+    echoerr "Error: you need to provide a HOST and PORT to test."
+    exit 1
+fi
+
+cmdname="entrypoint.sh"
+wait_for
+RESULT=$?
+if [[ $RESULT -ne 0 ]]; then
+    exit $RESULT
+fi
+
+echo "Starting API..."
+java -jar /app/ddev-0.0.1-SNAPSHOT.jar --spring.profiles.active=postgres
---
+++ defensive-programming/api/src/main/java/com/docker/ddev/controller/PetController.java
@@ -0,0 +1,45 @@
+package com.docker.ddev.controller;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.docker.ddev.model.Image;
+import com.docker.ddev.service.ImageService;
+import com.docker.ddev.util.CustomErrorType;
+
+@RestController
+@RequestMapping("/api")
+public class PetController {
+	public static final Logger logger = LoggerFactory.getLogger(PetController.class);
+	
+	@Autowired
+	ImageService imageService;
+
+	// -------------------Retrieve Single Image By Id------------------------------------------
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@RequestMapping(value = "/pet", method = RequestMethod.GET)
+	public ResponseEntity<?> getPet() {
+		logger.info("Fetching random Pet");
+        int max = 13;   // it would be better to have "SELECT count(*) FROM images" here
+        long imageId = ThreadLocalRandom.current().nextInt(1, max+1);
+		logger.info("Equals to fetching Image with Id {}", imageId);
+		Image image = imageService.findById(imageId);
+		if (image == null) {
+			logger.error("Image with id {} not found.", imageId);
+			return new ResponseEntity(new CustomErrorType("Image with id " + imageId 
+					+ " not found"), HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Image>(image, HttpStatus.OK);
+	}
+}
