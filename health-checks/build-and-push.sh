6faeb7798fad08e832811d06696bb43a656f130f
BillMills <mills.wj@gmail.com>
2019-05-14 14:26:32 -0400

    templates for health checks

--- health-checks/build-and-push.sh
+++ health-checks/build-and-push.sh
@@ -2,10 +2,10 @@ docker image build -t ddev_db:1.0 database
 docker image build -t ddev_api:1.0 api
 docker image build -t ddev_ui:1.0 ui
 
-docker image tag ddev_ui:1.0  $MY_DOCKER_ID/ddev_ui:1.0
-docker image tag ddev_api:1.0 $MY_DOCKER_ID/ddev_api:1.0
-docker image tag ddev_db:1.0  $MY_DOCKER_ID/ddev_db:1.0
+docker image tag ddev_ui:1.0  ${dockerID}/ddev_ui:1.0
+docker image tag ddev_api:1.0 ${dockerID}/ddev_api:1.0
+docker image tag ddev_db:1.0  ${dockerID}/ddev_db:1.0
 
-docker push $MY_DOCKER_ID/ddev_ui:1.0
-docker push $MY_DOCKER_ID/ddev_api:1.0
-docker push $MY_DOCKER_ID/ddev_db:1.0
+docker push ${dockerID}/ddev_ui:1.0
+docker push ${dockerID}/ddev_api:1.0
+docker push ${dockerID}/ddev_db:1.0
